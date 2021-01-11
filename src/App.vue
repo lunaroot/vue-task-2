073f9eec871cd9477e73df11167a8b2bad53a683
alexta <terpyakov@gmail.com>
2021-01-11 17:04:56 +0300

    add init

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,35 @@
 </template>
 
 <script>
+import axios from 'axios'
 import AppSettings from './AppSettings'
 import AppPortfolio from './AppPortfolio'
 import AppComments from './AppComments'
 
+const DB_URL = process.env.VUE_APP_DB_URL
+
 export default {
+  async mounted() {
+    await this.init()
+  },
+
+  methods: {
+    async init() {
+      try {
+        const { data } = await axios.get(`${DB_URL}portfolio.json`)
+        if (!data) {
+          const now = Date.now()
+          await axios.post(`${DB_URL}portfolio.json`, {
+            createdAt: now,
+            updatedAt: now
+          })
+        }
+      } catch (error) {
+        console.error('[init error]', error.message, Date.now())
+      }
+    }
+  },
+
   components: {
     AppSettings,
     AppPortfolio,
