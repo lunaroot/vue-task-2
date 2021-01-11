efeff5dfa6802d0afa35f63059083bdc142cfa2b
alexta <terpyakov@gmail.com>
2021-01-11 18:13:24 +0300

    review

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,50 @@
 <template>
   <div class="container column">
-    <app-settings></app-settings>
-    <app-portfolio></app-portfolio>
+    <app-portfolio :portfolio="portfolio"></app-portfolio>
   </div>
   <app-comments></app-comments>
 </template>
 
 <script>
 import axios from 'axios'
-import AppSettings from './AppSettings'
 import AppPortfolio from './AppPortfolio'
 import AppComments from './AppComments'
 
 const DB_URL = process.env.VUE_APP_DB_URL
 
 export default {
+  data() {
+    return {
+      portfolio: null
+    }
+  },
+
   async mounted() {
-    await this.init()
+    await this.loadPortfolio()
   },
 
   methods: {
-    async init() {
+    async loadPortfolio() {
       try {
         const { data } = await axios.get(`${DB_URL}portfolio.json`)
-        if (!data) {
+        if (data) {
+          this.portfolio = data
+        } else {
           const now = Date.now()
-          await axios.post(`${DB_URL}portfolio.json`, {
+          const portfolio = {
             createdAt: now,
             updatedAt: now
-          })
+          }
+          await axios.post(`${DB_URL}portfolio.json`, portfolio)
+          this.portfolio = portfolio
         }
       } catch (error) {
-        console.error('[init error]', error.message, Date.now())
+        console.error('[load portfolio error]', error.message, Date.now())
       }
     }
   },
 
   components: {
-    AppSettings,
     AppPortfolio,
     AppComments
   }
--- src/AppPortfolio.vue
+++ src/AppPortfolio.vue
@@ -1,4 +1,23 @@
 <template>
+  <form class="card card-w30">
+    <div class="form-control">
+      <label for="type">Тип блока</label>
+      <select id="type">
+        <option value="title">Заголовок</option>
+        <option value="subtitle">Подзаголовок</option>
+        <option value="avatar">Аватар</option>
+        <option value="text">Текст</option>
+      </select>
+    </div>
+
+    <div class="form-control">
+      <label for="value">Значение</label>
+      <textarea id="value" rows="3"></textarea>
+    </div>
+
+    <button class="btn primary">Добавить</button>
+  </form>
+
   <div class="card card-w70">
     <h1>Резюме Nickname</h1>
     <div class="avatar">
@@ -14,7 +33,7 @@
 
 <script>
 export default {
-
+  props: ['portfolio']
 }
 </script>
 
